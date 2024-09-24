88d21b12b49b1c20dcf63751e5876380403cf1a7
Voltage <bayibotu@gmail.com>
2024-09-24 18:23:29 +0300

    Add files via upload

---
+++ discord/index.php
@@ -0,0 +1,51 @@
+<link rel="icon" href="https://mc.luathesmc.com/assets/uploads/images/landing/logo/e9B10E9h1F9H7.png" type="image/x-icon" />
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+<title>LuathesMC Discord • McŞehri.NET</title>
+<style type="text/css" media="screen">
+html, body {
+position: absolute;
+height: 100%;
+max-height: 100%;
+width: 100%;
+margin: 0;
+padding: 0;
+}
+iframe {
+position: absolute;
+height: 100%;
+width: 100%;
+border: none;
+}
+#container {
+position: absolute;
+top: 50px; /* Change with Height of #header */
+bottom: 0;
+width: 100%;
+overflow: hidden ;
+}
+#header {
+position: absolute;
+top: 0px;
+height: 50px; /* Change with top of #container */
+color: #eee;
+background-color: #ccc;
+width: 100%;
+}
+</style>
+
+</head>
+
+<body>
+
+<iframe src="https://e.widgetbot.io/channels/1108269316446638111/1159919811816607784"></iframe>
+</div>
+
+</body>
+</html>
---
+++ js/5Gxkmd4FDiFRSZoIerfXU-_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/906a09f8-19a00b65e0bcaf8e.js","static/chunks/400-11de1be44b8c943d.js","static/chunks/pages/index-88068f2f54eafd53.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
